0db3db19213b6d339b8d56c313b80befb61af51e
AVIV NASTAIN S <143859817+apipns@users.noreply.github.com>
2023-09-10 15:24:59 +0700

    Add files via upload

---
+++ tugas 1 apipns 097.py
@@ -0,0 +1,13 @@
+#Latihan konversi satuan temperature
+ 
+# Program konversi Kelvin ke Celcius
+
+print("\nProgram Konversi Kelvin\n")
+
+Kelvin = float(input("Masukkan suhu dalam Kelvin = "))
+print("Suhu  Kelvin Adalah =",Kelvin, "Kelvin")
+
+#Celcius
+Celcius = Kelvin - 273.15
+print ("Konversi dari",Kelvin,"K ke Celcius adalah : ",Celcius,"C" )
+
---
+++ tugas 2. apipns 097.py
@@ -0,0 +1,12 @@
+#Latihan konversi satuan temperature
+ 
+# Program konversi Celcius ke Fahrenheit
+
+print("\nProgram Konversi Celcius\n")
+
+celcius = float(input("Masukkan suhu dalam Celcius = "))
+print("Suhu  Celcius Adalah =",celcius, "Celcius")
+
+#Fahrenheit
+Fahrenheit = ((9/5) * celcius) + 32
+print ("Konversi dari",celcius,"C ke Fahrenheit adalah : ",Fahrenheit,"F" )
